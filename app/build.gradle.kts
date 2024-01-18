81329c6a51dbdcf02a1add09d6093632732d0e89
Liudvikas Sablauskas <liudvikas.sablauskas@telesoftas.com>
2024-01-18 18:32:04 +0200

    Bump target version to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.ar_ka_rodo_kine"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.ar_ka_rodo_kine"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
